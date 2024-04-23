9dd483c4561575f5ac4a6e3dbb03b015db51315f
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2024-04-23 14:38:41 -0700

    require http module

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello Node!\n');
+}).listen(8080);
