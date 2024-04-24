e47dc3a002dcab49b675c11e36a5fb745b23f8ad
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2024-04-24 11:48:13 -0700

    end response

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ http.createServer((request, response) => {
         
         response.writeHead(200, { "Content-Type": "text/html" });
         response.write(data);
+        response.end();
+
 
     })
 
-    response.end('Hello Node!\n');
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
