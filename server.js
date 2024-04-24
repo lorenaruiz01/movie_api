1eb11209235fd4a223de21965eb6e05513c3b684
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2024-04-24 11:36:11 -0700

    create server with requested url

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http'),
     url = require('url');
 
 http.createServer((request, response) => {
+    let addr = "request.url";
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello Node!\n');
 }).listen(8080);
@@ -10,4 +11,3 @@ http.createServer((request, response) => {
 console.log('My first Node test server is running on Port 8080.');
 
 
-let addr = "request.url";
