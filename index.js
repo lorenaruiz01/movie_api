3f1b448ae100b11744701c1d199290336628f9b8
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2024-05-07 12:17:24 -0700

    route endpoint to public folder

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+    morgan = require('morgan');
 const app = express();
+
+app.use(morgan('common'))
 
 app.get('/', (req, res) => {
     res.send('Welcome to mySpookyFlix! A place for spooky movies -but not too scary!')
 });
 
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname})
+});
+app.use(express.static("public"));
+
+
 app.get('/movies', (req, res) => {
     res.json('movies');
 });
 
-app.use(express.static('public'));
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
