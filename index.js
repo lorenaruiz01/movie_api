23a4eb1b0263c687892d387478451a6e1a6e45ad
Lorena Ruiz <101694059+lorenaruiz01@users.noreply.github.com>
2024-05-07 13:16:16 -0700

    listen on port 8080

--- index.js
+++ index.js
@@ -22,4 +22,8 @@ app.get('/movies', (req, res) => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Oops! Something broke!')
-})
+});
+
+app.listen(8080, () => {
+    console.log('your app is listening on port 8080.');
+});
